46eee313ca5e5db1346bf651982e1c144540df88
monjur <monjurmorshed793@gmail.com>
2019-04-03 01:14:32 +0600

    random changes.

--- src/main/java/org/sofl/soptorshi/client/DepartmentView.java
+++ src/main/java/org/sofl/soptorshi/client/DepartmentView.java
@@ -1,9 +1,15 @@
 package org.sofl.soptorshi.client;
 
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.sofl.soptorshi.MainView;
+import org.springframework.security.access.annotation.Secured;
 
+@Secured({"ROLE_USER"})
 @Route(value = "department", layout = MainView.class)
 public class DepartmentView extends VerticalLayout {
+    public DepartmentView() {
+        add(new Label("Department view"));
+    }
 }
